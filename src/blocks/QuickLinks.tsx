143487ae40210449dc68812f327ac0da3aea26c7
Gimerbone <gimerbone@gmail.com>
2025-11-18 20:53:15 +0700

    Adjusting icons

--- src/blocks/QuickLinks.tsx
+++ src/blocks/QuickLinks.tsx
@@ -1,16 +1,18 @@
-import { type LucideIcon } from "lucide-react"
-import { ArrowUpRight, Users, ClipboardList, CalendarClock, BusFront, Shirt, MessageCircleMore } from "lucide-react";
+import { ScrollText, type LucideIcon } from "lucide-react"
+import { Clapperboard, ArrowUpRight, Users, ClipboardList, BookAlert, BusFront, PencilRuler, MessageCircleMore } from "lucide-react";
 
 export default function QuickLinks() {
     return (
         <div className="relative bg-white w-full py-24 md:py-32 flex flex-col items-center justify-center md:gap-12 gap-10">
             <h2 className="relative lg:text-4xl md:text-3xl text-2xl font-bold">Pusat Informasi</h2>
             <div className="relative rounded-4xl w-screen md:w-230 p-4 md:p-8 bg-white flex flex-wrap justify-center gap-4 md:gap-6">
-                <LinkContainer icon={CalendarClock} title = "Rundown Acara" link="https://drive.google.com/file/d/1_d18TW8VXJrGArGUocX0ZRn5v1VN1CHG/view?usp=drive_link"/>
+                <LinkContainer icon={ClipboardList} title = "Rundown Acara" link="https://drive.google.com/file/d/1_d18TW8VXJrGArGUocX0ZRn5v1VN1CHG/view?usp=drive_link"/>
                 <LinkContainer icon={BusFront} title = "Denah Kursi Bus" link="https://drive.google.com/file/d/1lTA9_5Uba2w7PD9IEwmdpDDiik_IiW9R/view?usp=drive_link"/>
                 <LinkContainer icon={Users} title = "Tim Panitia" link="https://example.com/"/>
-                <LinkContainer icon={ClipboardList} title = "Template Dispen" link="https://example.com/"/>
-                <LinkContainer icon={Shirt} title = "Perlengkapan" link="https://example.com/"/>
+                <LinkContainer icon={BookAlert} title = "Template Dispen" link="https://example.com/"/>
+                <LinkContainer icon={ScrollText} title = "Peraturan" link="https://example.com/"/>
+                <LinkContainer icon={PencilRuler} title = "Perlengkapan" link="https://example.com/"/>
+                <LinkContainer icon={Clapperboard} title = "Tanya Peserta" link="https://example.com/"/>
                 <LinkContainer icon={MessageCircleMore} title = "FAQ" link="https://example.com/"/>
             </div>
         </div>
